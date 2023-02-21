a0c24f3b56162b84d0772acb7abf31a903f432fa
Radu Mojic <radu.mojic@elrond.com>
2023-02-21 16:13:16 +0200

    [+] temporarily allow both elrond and multiversx namespaces for an easier migration from Maiar App to xPortal App

--- src/operation.ts
+++ src/operation.ts
@@ -1,4 +1,11 @@
 export enum Operation {
+  SIGN_TRANSACTION = "multiversx_signTransaction",
+  SIGN_TRANSACTIONS = "multiversx_signTransactions",
+  SIGN_MESSAGE = "multiversx_signMessage",
+  SIGN_LOGIN_TOKEN = "multiversx_signLoginToken",
+}
+
+export enum OldOperation {
   SIGN_TRANSACTION = "erd_signTransaction",
   SIGN_TRANSACTIONS = "erd_signTransactions",
   SIGN_MESSAGE = "erd_signMessage",
