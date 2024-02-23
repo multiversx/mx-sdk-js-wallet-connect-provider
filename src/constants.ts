2c8e8d261aacee5a8d6ee1c4f2bd6103a4814360
Radu Mojic <radu.mojic@multiversx.com>
2024-02-23 16:21:41 +0200

    - extend the timeout to 600ms to allow local processing on slower devices on both ends

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,5 @@ export const WALLETCONNECT_MULTIVERSX_METHODS = Object.values(Operation);
 // WalletConnect optional methods for Multiversx
 export const WALLETCONNECT_MULTIVERSX_OPTIONAL_METHODS =
   Object.values(OptionalOperation);
-// Delay the sign login token action for 1000ms to allow the relay to update properly
-export const WALLETCONNECT_SIGN_LOGIN_DELAY = 1000;
+// Delay the sign login token action for 600ms to allow the relay to update properly
+export const WALLETCONNECT_SIGN_LOGIN_DELAY = 600;
