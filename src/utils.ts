e1c550078142a56edb630626600694bf8bc3f7b4
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-11-24 11:22:36 +0200

    Update sdk-core imports

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { Address, Transaction } from "@multiversx/sdk-core";
+import { Address } from "@multiversx/sdk-core/out/core/address";
+import { Transaction } from "@multiversx/sdk-core/out/core/transaction";
 import Client from "@walletconnect/sign-client";
 import {
   EngineTypes,
